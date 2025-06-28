c88096b07ed1f376aff9996698be4b8388c1459b
Matthew Biskas <matthewbiskas@gmail.com>
2025-06-28 14:32:43 +0200

    restarting work

--- src/common/cpuinfo.h
+++ src/common/cpuinfo.h
@@ -1,2 +1,8 @@
 // SPDX-FileCopyrightText: Copyright 2024 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
+
+class CPUInfo {
+    public:
+        static std::string Vendor(void) { return CPU_Rep.vendor_; }
+        static std::string Brand(void) { return CPU_Rep.brand_; }
+}
